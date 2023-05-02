e7ef135aedec4c9b52fab9c980f17ff2522415b7
Gear <gear@gear.is>
2023-05-02 02:17:33 -0400

    Add licensing to the project

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,19 @@
+// The main entry point of the program. This file varies by iteration with its purpose.
+// Copyright (C) 2023 Gear
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 const std = @import("std");
 pub const tuples = @import("tuples.zig");
 
